bcca3b51ce8c0d4fc8575975d3213e1cd42fbe9f
alepaillas <alepaillas@gmail.com>
2025-06-10 21:19:35 -0400

    update dotenv code

--- src/utils/dotenv.ts
+++ src/utils/dotenv.ts
@@ -1,11 +1,17 @@
 import * as dotenv from "dotenv";
 import * as path from "path";
+import { fileURLToPath } from 'url';
 
-const envPath = path.resolve(".env");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const envPath = path.resolve(__dirname, "../../relbase-api/.env");
+// console.log(envPath);
 dotenv.config({ path: envPath });
 
 // API configuration
 export const base_url = `${process.env.BASE_URL}`;
+// console.log(base_url);
 export const headers = {
   Authorization: `${process.env.USER_TOKEN}`,
   Company: `${process.env.ENTERPRISE_TOKEN}`,
